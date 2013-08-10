7341ada29e5f8ab289fb93aec46baf5f1d0d9009
Bruno Vernier <bruno.vernier@gmail.com>
2013-08-09 17:18:51 -0700

    bug fix: unknown user_ids

--- check_user_ids.php
+++ check_user_ids.php
@@ -8,13 +8,23 @@
     $tid =  $row['tid'];
     $currency =  $row['currency'];
     $userid =  $row['user_id'];
-    $trading = $row['trading_account'];
-    $with = $row['with_account'];
+    $trading = trim($row['trading_account']);
+    $with = trim($row['with_account']);
     $report = exec_sql("select * from FULL_QUERY where trading_name=? and currency=?",array($trading, $currency),"details of full query");
     //$report2 = exec_sql("select * from user_account_currencies where trading_name=? ",array($trading),"");
-    $check = " <br><font color=red><b>No Match</b></font> for tid:$tid $trading ($with) using $currency in uac ".count($report);
+    if (count($report)==0) {
+      $c_id = exec_sql("select id from currencies where currency=?",array($currency),'',1);
+      $uac_usid = exec_sql("select user_space_id from user_account_currencies where trading_name=? and currency_id=?",
+                  array($trading, $c_id),'',1);
+      $us_sid = exec_sql("select space_id from user_spaces where id = ?",array($uac_usid),'',1);
+      $s_id = exec_sql("select id from spaces where id=?",array($us_sid),'',1);
+      echo "<br>select user_space_id from user_account_currencies where trading_name=$trading and currency_id=$c_id";
+      echo "<br>uac_usid=$uac_usid, us_sid=$us_sid, s_id=$s_id, c_id=$c_id";
+    }
+    $check = " <br><font color=red><b>No id Match</b></font>tid=$tid <b>$trading</b> -> $with in $currency";
     foreach ($report as $row2) {
       $uid = $row2['user_id'];
+      $userid = $userid?$userid:$uid;
       $account = $row2['user_name'];
       if ($uid == $userid AND $userid>'') {
         $check=" <b>.</b>";
@@ -28,7 +38,7 @@
       } else {
         //echo "<br>$currency $tid $trading $userid";
         $check = '';
-        echo " <br><font color=red><b>(MISMATCH)</b></font>";
+        echo " <br><font color=red><b>(MISMATCH $userid<>$uid) </b></font>";
         echo "... UPDATE user_journal set user_id='$uid' where  trading_account='$trading' and currency='$currency'";
         $update = exec_sql("update user_journal set user_id=? where  trading_account=? and currency=?",array($uid, $trading, $currency),
 			   "updating user_journal with correct user_id $uid",2);      
