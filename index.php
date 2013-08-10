069232b1b5442a24f6f648815c25989c6adb25dc
Bruno Vernier <bruno.vernier@gmail.com>
2013-08-10 14:38:05 -0700

    added table structure

--- index.php
+++ index.php
@@ -9,25 +9,28 @@ require('header.php');
 <br>   
   
     <!--h1>Login</h1-->
-
     <form action="login.php" method="post">
-      <label for="username">Username</label>
-      <input type="text" id="username" name="username" autofocus placeholder='<email address>' />
-
-      <br />
-
-      <label for="password">Password</label>
-      <input type="password" id="password" name="password" required placeholder='<password>'/>
-
-      <br />
-
-      <input type="submit" value="Login" />
-    </form>
+    <table class="front">
+  <tr>
+    <td><label for="username">Username</label></td>
+    <td><input type="text" id="username" name="username" autofocus placeholder='<email address>' /></td>
+  </tr>
+  <tr>
+    <td><label for="password">Password</label></td>
+    <td><input type="password" id="password" name="password" required placeholder='<password>'/></td>
+  </tr>
+  <tr>
+    <td colspan="2" align="center"><input type="submit" value="Login" /></td>
+    </tr>
+</table>
+    <br />
     <a href="signup.php">Request a new account</a>
    <br> <a href="signup.php?newpw=1">Request a new password</a>
    <br>
    <br> <a href="http://tinyurl.com/mjz4wbm" target="_blank">user guide to open money</a>
-  </div>
+  </form>
+ </div>
+</div>
 </center>
 
 
