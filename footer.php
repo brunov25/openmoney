f12288f5302d7470271ef0e8573ba2ad2e09ab66
Bruno Vernier <bruno.vernier@gmail.com>
2013-08-10 14:39:16 -0700

    added copyleft notice

--- footer.php
+++ footer.php
@@ -2,6 +2,8 @@
 <? 
 $user_name = isset($_SESSION['user_name'])?$_SESSION['user_name']:'';
 $feedback=$user_name?"<a href=feedback.php><font size=-1>feedback</font></a>":'';
-echo "<p>$feedback</center></div></body></html>";
+$copyleft="<br><font size=-2 style='color:#CCC;'>&copy; 2006-2013 GPL by Open Money Development Group</font>";
+
+echo "<center><p>$feedback $copyleft</center></div></body></html>";
 ?>
 </div>
