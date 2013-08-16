75279afc386c5f666a518a230e12c964a1f40922
Bruno Vernier <bruno.vernier@gmail.com>
2013-08-16 16:01:51 -0700

    extraneous /DIV

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
-<div class="modal-footer">
 <? 
+echo "<div class='modal-footer'>";
 $user_name = isset($_SESSION['user_name'])?$_SESSION['user_name']:'';
 $feedback=$user_name?"<a href=feedback.php><font size=-1>feedback</font></a>":'';
 $copyleft="<br><font size=-2 style='color:#CCC;'>&copy; 2006-2013 GPL by Open Money Development Group</font>";
 
 echo "<center><p>$feedback $copyleft</center></div></body></html>";
 ?>
-</div>
