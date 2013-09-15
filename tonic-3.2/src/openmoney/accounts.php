384b588f122ca95068a840af53f92120453ac09a
deefactorial <deefactorial@gmail.com>
2013-09-14 18:58:12 -0700

    transaction details

--- tonic-3.2/src/openmoney/accounts.php
+++ tonic-3.2/src/openmoney/accounts.php
@@ -449,3 +449,150 @@ class accountHistory extends Resource
 }
 
 
+
+/**
+ * The @uri annotation routes requests that match that URL to this resource. Multiple
+ * annotations allow this resource to match multiple URLs.
+ *
+ * @uri /accounts/transferData/:transactionID
+ */
+class accountTransferData extends Resource
+{
+
+	private $user;
+	private $username;
+
+	/**
+	 * The setup() method is called when the resource is executed. We don't do this check
+	 * within the resource constructor as we can't cleanly throw an exception from within
+	 * an object constructor.
+	 */
+	function setup() {
+		require("rest_connect.php");
+		require("../password.php");
+		if(isset($_SERVER['PHP_AUTH_USER'])&&isset($_SERVER['PHP_AUTH_PW'])){
+
+			//echo "Attempting to authenticate user.\n";
+			$users_q = mysqli_query($db,"SELECT * FROM users WHERE user_name='" . mysqli_real_escape_string($db,$_SERVER['PHP_AUTH_USER']) ."'");
+			$users = mysqli_fetch_array($users_q);
+			//echo "username:".$users['user_name']."\n";
+			//echo "password:".$_SERVER['PHP_AUTH_PW']."\n";
+			//echo "check1".password_verify($_SERVER['PHP_AUTH_PW'],$users['password2'])."\n";
+			//echo "check2".password_verify($_SERVER['PHP_AUTH_PW'],$users['password'])."\n";
+			if(password_verify($_SERVER['PHP_AUTH_PW'],$users['password2']) OR (password_verify($_SERVER['PHP_AUTH_PW'], $users['password']))){
+				//echo "verified\n";
+				$this->user = $users;
+				$this->username = $users['user_name'];
+			} else {
+				throw new Tonic\UnauthorizedException;
+			}
+		} else {
+			throw new Tonic\UnauthorizedException;
+		}
+	}
+
+	/**
+	 * Use this method to handle GET HTTP requests.
+	 *
+	 * The optional :accountID parameter in the URL available as the first parameter to the method
+	 * or as a property of the resource as $this->name.
+	 *
+	 * Method can return a string response, an HTTP status code, an array of status code and
+	 * response body, or a full Tonic\Response object.
+	 *
+	 * @method GET
+	 * @param  str $accountID
+	 * @provides application/json
+	 * @json
+	 * @return Tonic\Response
+	 */
+	public function sayAccountsTransferData($transactionID = 0)
+	{
+		if($transactionID != 0){
+			require("rest_connect.php");
+			
+			$accounts_a xrray = array();
+			$accounts_q = mysqli_query($db,$test = "SELECT *, uac.id user_account_currencies_id FROM user_account_currencies uac, user_spaces us, currencies c  WHERE uac.id='".mysqli_real_escape_string($db, intval($accountID))."' AND uac.currency_id=c.id AND uac.user_space_id=us.id AND us.user_id='".$this->user['id']."'") or die($test . mysqli_error($db));
+			while($accounts = mysqli_fetch_array($accounts_q)){
+
+				$totalCount = 0;
+				$balance = 0.00000;
+				
+				$elements_array = array();
+				
+				$user_journal_q = @mysqli_query($db,$test = "SELECT * FROM user_journal WHERE id='".$transactionID."' AND user_id='".$this->user['id']."' ORDER BY tid DESC LIMIT $currentStartEntry, $pageSize ");
+				while($user_journal = @mysqli_fetch_array($user_journal_q)){
+					$totalCount++;
+					$balance = floatval($user_journal['balance']);
+					$balance_decimal = number_format($balance,2);
+					$trading_name_q = @mysqli_query($db, $test = "SELECT * FROM user_account_currencies WHERE trading_name='".$user_journal['trading_account']."'");
+					$trading_name = @mysqli_fetch_array($trading_name_q);
+					$account_name_q = @mysqli_query($db, $test = "SELECT * FROM user_account_currencies WHERE trading_name='".$user_journal['with_account']."'");
+					$account_name = @mysqli_fetch_array($account_name_q);
+					
+					array_push($elements_array, array("id"=>$user_journal['id'],
+													   "date"=>date("Y-m-d\TH:i:s",$user_journal['tid']).".000+0000",
+													   "formattedDate"=>date("Y-m-d",$user_journal['tid']),
+													   "processDate"=>date("Y-m-d\TH:i:s",$user_journal['tid']).".000+0000",
+													   "formattedProcessDate"=>date("Y-m-d",$user_journal['tid']),
+													   "amount"=>floatval($user_journal['amount']),
+													   "formattedAmount"=>$user_journal['amount']." ".$accounts['currency'],
+													   "transferType"=>array("id"=>$accounts['currency_id'],
+																			"name"=>$accounts['currency']." exchange",
+																			"from"=>array("id"=>$trading_name['id'],
+																						  "name"=>$user_journal['trading_account'],
+																						  "currency"=>array("id"=>$accounts['currency_id'],
+																											"symbol"=>$accounts['currency'],
+																											"name"=>$accounts['currency'])),
+																			"to"=>array("id"=>$account_name['id'],
+																						  "name"=>$user_journal['with_account'],
+																						  "currency"=>array("id"=>$accounts['currency_id'],
+																											"symbol"=>$accounts['currency'],
+																											"name"=>$accounts['currency']))),
+														"description"=>$user_journal['description']));
+				}
+
+				$default = false;
+				if($accounts['currency_id']==1)
+					$default = true;
+				$accounts_array = array( "currentPage" => $currentPage,
+						"pageSize" => $pageSize,
+						"totalCount" => $totalCount,
+						"elements" => $elements_array);
+
+
+			}
+			$result = new Response(200, $accounts_array);
+		} else {
+			throw new Tonic\NotFoundException;
+		}
+		return $result;
+	}
+
+
+	/**
+	 * Condition method to turn output into JSON.
+	 *
+	 * This condition sets a before and an after filter for the request and response. The
+	 * before filter decodes the request body if the request content type is JSON, while the
+	 * after filter encodes the response body into JSON.
+	 */
+	protected function json()
+	{
+		$this->before(function ($request) {
+			if ($request->contentType == "application/json") {
+				$request->data = json_decode($request->data);
+			}
+		});
+		$this->after(function ($response) {
+			$response->contentType = "application/json";
+			if (isset($_GET['jsonp'])) {
+				$response->body = $_GET['jsonp'].'('.json_encode($response->body).');';
+			} else {
+				$response->body = json_encode($response->body);
+			}
+		});
+	}
+}
+
+
