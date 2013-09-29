e0fdc5226e065637e011c46adffb91704fca06d1
Bruno Vernier <bruno.vernier@gmail.com>
2013-09-29 13:47:14 -0700

    better sql

--- report.php
+++ report.php
@@ -5,10 +5,11 @@ if (!is_admin()) { header('main.php');}
 
 $currency=isset($_REQUEST[ 'currency' ])? $_REQUEST['currency']:'';
 
-$last_transactions = exec_sql("select *,currency, trading_account, trading from user_journal j where currency=? and trading = (select max(trading) from user_journal j2 where j2.trading_account = j.trading_account); ",array($currency),"items in journal",0);
+$last_transactions = exec_sql("select *,currency, trading_account, trading from user_journal j where currency=? and trading = (select max(trading) from user_journal j2 where j2.trading_account = j.trading_account) order by trading_account,id desc; ",array($currency),"items in journal",0);
 
 $data = '';
-
+$total = 0.00;
+$total2 = 0.00;
 foreach ($last_transactions as $last) {
   $curr = $last['currency'];
   $user_id = $last['user_id'];
@@ -17,6 +18,8 @@ foreach ($last_transactions as $last) {
   $trading = $last['trading'];
   $date = $last['created'];
   $balance = $last['balance'];
+  $total = $total + $balance;
+  $total2 = $total2 + $trading;
   $amount = $last['amount'];
   $tid = $last['tid'];
   $description = $last['description'];
@@ -24,8 +27,8 @@ foreach ($last_transactions as $last) {
   $data .="<tr><td>{$curr} {$date} {$user_name} ($user_id)</td><td> {$trading_account} -> {$with_account}</td>
            <td>$tid: $description </td>
            <td align=right>$amount</td>
-           <td align=right>$balance</td>
-           <td align=right bgcolor=cyan>$trading</td>
+           <td align=right bgcolor=#ffffe>$balance</td>
+           <td align=right bgcolor=bisque>$trading</td>
   </tr>";
 }
 
@@ -36,7 +39,9 @@ foreach ($all_currencies as $cur) {
   $currencies .= "<option value='{$curr}'>{$curr}</option>";
 }
 
-echo"<form><h3>{$CFG->site_name} OM Report for $currencies</select><input type=submit value=change></h3><table border>$data</table></form>";
+echo"<form><h3>{$CFG->site_name} OM Report for $currencies</select>
+       <input type=submit value=change></h3><table border>$data
+       </tr><th colspan=4> Total Balance = </th><th align=right>$total</th><th align=right bgcolor=bisque>$total2</th></tr></table></form>";
 
 require('footer.php');
 ?>
