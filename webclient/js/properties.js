3efeae61a4fb48c5b23ba8ecee55e64b4ab9f0ce
deefactorial <deefactorial@gmail.com>
2013-10-22 21:16:47 -0700

    additional files

---
+++ webclient/js/properties.js
@@ -0,0 +1,35 @@
+/*
+    This file is part of Cyclos (www.cyclos.org).
+    A project of the Social Trade Organisation (www.socialtrade.org).
+
+    Cyclos is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Cyclos is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cyclos; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ */
+
+/* 
+ * Cyclos Mobile Configuration
+ * 
+ * Possible options
+ * hostUrl: "http://www.cyclos.org/cyclos_en"
+ * sessionTimeout: 300 (seconds) 
+ * splashScreen: true (true/false)
+ * version: "1.0.0"
+ */
+var options = {
+		version: "1.1.1",
+		sessionTimeout: 1800,
+		splashScreen: false
+		
+};
